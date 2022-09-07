4a5d5dea845eebb1d6cda6126f497d68c7cb7268
Denis Malin <denis@malina.page>
2022-09-07 19:47:07 +0400

    fix version file

--- aio_clients/__version__.py
+++ aio_clients/__version__.py
@@ -1,9 +1,19 @@
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
+import sys
+from typing import Any
+
+if sys.version_info < (3, 10):
+    # compatibility for python <3.10
     import importlib_metadata as metadata
+else:
+    from importlib import metadata
+
 from collections.abc import Callable
 
-version: Callable[[str], str] = metadata.version
+try:
+    VERSION_TYPE = Callable[[str], str]
+except TypeError:
+    VERSION_TYPE = Any  # type: ignore
+
+version: VERSION_TYPE = metadata.version
 
 __version__ = version("aio-clients")
