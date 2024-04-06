6e1a5879d3e1f6c09b1d74b5155d76a2d869fc0d
Denis Malin <denis@malina.page>
2024-04-06 09:12:33 +0700

    fix typing

--- aio_clients/types.py
+++ aio_clients/types.py
@@ -1,29 +1,31 @@
-import sys
-from typing import Dict, Any, Awaitable, Optional, Union, Tuple, List
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol
-else:
-    Protocol = object
+from typing import Dict, Any, Optional, Union, Tuple, List
 
 Q_PARAMS_TYPE = Optional[Union[
     Dict[str, Union[str, int]],
     List[Tuple[str, Union[str, int]]]
 ]]
 
+# I don't know how to write this type
+# for async function with kwargs
+# FIXME maybe i fix this type later
+# class MiddlewareStart(Protocol):
+#     def __call__(
+#             self,
+#             headers: Optional[Dict[str, str]],
+#             request_kwargs: Optional[Dict[str, Any]],
+#             **kwargs: Any
+#     ) -> Awaitable[None]: pass
 
-class MiddlewareStart(Protocol):
-    def __call__(
-            self,
-            headers: Optional[Dict[str, str]] = None,
-            request_kwargs: Optional[Dict[str, Any]] = None,
-            **kwargs
-    ) -> Awaitable[None]: pass
+MiddlewareStart = Any
 
+# I don't know how to write this type
+# for async function with kwargs
+# FIXME maybe i fix this type later
+# class MiddlewareEnd(Protocol):
+#     def __call__(
+#             self,
+#             response,
+#             **kwargs
+#     ) -> Awaitable[None]: pass
 
-class MiddlewareEnd(Protocol):
-    def __call__(
-            self,
-            response,
-            **kwargs
-    ) -> Awaitable[None]: pass
+MiddlewareEnd = Any
